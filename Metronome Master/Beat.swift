e0e07affb70f8e1227e9e19c26dc025b45c4b14f
Andrew Paterson <andrewpaterson@Andrews-MacBook-Air.local>
2015-05-10 15:50:52 -0400

    Skeleton for model

---
+++ Metronome Master/Beat.swift
@@ -0,0 +1,13 @@
+//
+//  Beat.swift
+//  Metronome Master
+//
+//  Created by Andrew Paterson on 5/10/15.
+//  Copyright (c) 2015 Andrew Paterson. All rights reserved.
+//
+
+import UIKit
+
+class Beat: NSObject {
+   
+}
---
+++ Metronome Master/Measure.swift
@@ -0,0 +1,14 @@
+//
+//  Measure.swift
+//  Metronome Master
+//
+//  Created by Andrew Paterson on 5/10/15.
+//  Copyright (c) 2015 Andrew Paterson. All rights reserved.
+//
+
+import UIKit
+
+class Measure: NSObject {
+   
+}
+
